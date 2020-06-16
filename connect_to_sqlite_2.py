2fcb6ef3fc67389aa74fed89aa6aaea8ad95e712
javadebadi <javad.ebadi.1990@gmail.com>
2020-06-16 10:33:56 -0700

    Add
    Add: added connect_to_sqlite_2.py which is an example how to reflect metadata from table

---
+++ connect_to_sqlite_2.py
@@ -0,0 +1,31 @@
+# Import create_engine, MetaData, Table
+# the MetaData object will be filled with metadata of a table
+# the Table object will get the metadata of a table from database
+from sqlalchemy import create_engine, MetaData, Table
+
+# path to databases location in windows
+driver_database_name = "sqlite:///"
+path_to_dbs = "C:\\Users\\Javad\\Documents\\Github\\SQL_training\\databases\\"
+db_name = "census.sqlite"
+connection_string = driver_database_name + path_to_dbs + db_name
+
+# Create an engine that connects to the census.sqlite file: engine
+engine = create_engine(connection_string)
+
+# Create a metadata object: metadata
+metadata = MetaData()
+
+# Reflect census table from the engine: census
+census = Table('census', metadata, autoload=True, autoload_with=engine)
+
+
+# Print table names and information about table
+print("Database: {}".format(db_name))
+print("Tables in the database:")
+print(engine.table_names())
+
+
+
+# Print census table metadata
+print("Print metadata:")
+print(repr(census))
