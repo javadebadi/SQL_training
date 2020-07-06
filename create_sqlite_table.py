3654e57b1f36e99fc1b56fa6be197ab6629f83a6
javadebadi <javad.ebadi.1990@gmail.com>
2020-07-06 02:37:54 -0700

    Add: added an script to create an sqlite table using sqlalchemy

---
+++ create_sqlite_table.py
@@ -0,0 +1,25 @@
+from paths import *
+from sqlalchemy import create_engine, MetaData
+from sqlalchemy import Table, Column, String, Integer, Float, Boolean
+
+# get connection string
+connection_string = get_connection_string("census.sqlite")
+# create engine
+engine = create_engine(connection_string)
+
+# create metadata objec
+metadata = MetaData()
+
+# Define a new table with a name, count, amount, and valid column: data
+new_data = Table('new_data', metadata,
+            Column('name', String(255), unique=True),
+            Column('count', Integer(), default=1),
+            Column('amount', Float()),
+            Column('valid', Boolean(), default=False)
+)
+
+# Use the metadata to create the table
+metadata.create_all(engine)
+
+# Print table details
+print(repr(new_data))
